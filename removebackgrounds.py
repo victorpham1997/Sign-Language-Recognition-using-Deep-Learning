652b9f7472922223a9aa7193430baf69749c5e06
Elliot Koh <appdev.moose@gmail.com>
2021-04-27 17:21:40 +0800

    Added remove bg script

---
+++ removebackgrounds.py
@@ -0,0 +1,63 @@
+import torch
+from segmentation import segment_img_rm_bg
+from torch.nn import functional as F
+import cv2
+import torchvision
+from glob import glob
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+device = torch.device('cpu')
+# device = torch.device('cuda')
+
+
+deeplabmodel = torch.hub.load('pytorch/vision:v0.6.0', 'deeplabv3_resnet101', pretrained=True).to(device).eval()
+
+dataset_path = './dataset/'
+folders = ['train', 'val']
+
+
+def segment_and_replace_video(filepath, out_dir):
+    filename = os.path.basename(filepath)
+    cap = cv2.VideoCapture(filepath)   # capturing the video from the given path    
+    video_arr = []
+    out = None
+    while(cap.isOpened()):
+        frameId = cap.get(1) #current frame number
+        ret, frame = cap.read()
+        
+        frame_interval = 2
+        if not out:
+            fourcc = cv2.VideoWriter_fourcc(*'mp4v')            
+            #0x7634706d
+            fps = cap.get(cv2.CAP_PROP_FPS)
+            print(f"out: {f'{out_dir}/{filename[:-4]}.mp4'}")
+            out = cv2.VideoWriter(f'{out_dir}/{filename[:-4]}.mp4', fourcc, int(fps/frame_interval), (256, 256))
+
+        if (ret != True):
+            break
+        if (frameId % frame_interval == 0):
+            # Converting to tensor
+            frame =  torchvision.transforms.functional.to_tensor(frame).float().to(device)
+            frame = frame.unsqueeze(0)
+            frame =  F.interpolate(frame, (256,256), mode='bilinear')
+            frame = frame.squeeze(0)
+            frame_nobg = segment_img_rm_bg(frame, deeplabmodel, device)
+            # print(frame_nobg.shape)
+            # plt.imshow(frame_nobg)
+            # plt.show()
+            # break
+            # video_arr.append(frame_nobg)
+            # print(frame.shape)
+            out.write((frame_nobg*255).astype(np.uint8))
+    
+    cap.release()    
+    if out:
+        out.release()    
+
+
+for folder in folders:
+    names = glob(f'{dataset_path}{folder}/*[!_nobg].mp4')
+    for name in names:
+        segment_and_replace_video(name, f'{dataset_path}nobg/{folder}')
+        print(name)
--- segmentation.py
+++ segmentation.py
@@ -35,3 +35,32 @@ def segment_rm_bg(video, segmentation_model, device):
     maskedvideo = copy.deepcopy(video.permute(0,2,3,1)) #convert to b,w,h,c
     maskedvideo[maskbool] = 0 #set images outside mask to 0
     return torch.tensor(maskedvideo)
+
+
+def segment_img_rm_bg(frame, segmentation_model, device):
+    """
+    input video is a tensor
+    output video is a tensor
+    """
+    imagenet_stats = [[0.485, 0.456, 0.406], [0.229, 0.224, 0.225]]
+
+    preprocess = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),
+                                                torchvision.transforms.Normalize(mean = imagenet_stats[0],
+                                                                                std  = imagenet_stats[1])])
+
+    def segmentimg(img):
+        input_tensor = preprocess(img).unsqueeze(0)
+        input_tensor = input_tensor.to(device)
+
+        with torch.no_grad():      
+            output = segmentation_model(input_tensor)["out"][0]
+            output = output.argmax(0)
+        return output
+    
+    npframe = frame.permute(1,2,0).cpu().numpy()
+    mask = segmentimg(npframe).cpu().numpy()
+    # print(np.unique(mask))
+    maskbool = (mask!=15)
+    maskbool = np.repeat(maskbool[:, :, np.newaxis], 3, axis=2) # (512,512) mask to (512,512,3)
+    npframe[maskbool] = 0
+    return npframe
